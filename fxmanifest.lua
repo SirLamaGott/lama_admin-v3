7420926fd8eecf9543463efea163945db0532bb9
John <johnbronnert@gmail.com>
2022-05-17 08:59:27 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'SirLamaGott'
+description 'Lama - Admin v2'
+version '0.2.0'
+
+shared_script 'config.lua'
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'server/main.lua'
+}
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'client/main.lua'
+}
+dependencies {
+	'es_extended'
+} 
+
+escrow_ignore {
+  'config.lua',
+  'client/*.lua',
+  'server/*.lua',
+  'locales/*.lua'
+}
